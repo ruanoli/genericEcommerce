6e1813b8b7446838306fb73149ddf72b7022da2b
Ruan Oliveira <ruanoliveira.pro@gmail.com>
2024-04-24 18:06:06 -0300

    initial structure

---
+++ GenericEcommerce/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace GenericEcommerce.Models
+{
+    public class Category
+    {
+        public int IdCategory { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+
+        public IList<Game> Games { get; set; }
+    }
+}
---
+++ GenericEcommerce/Models/Game.cs
@@ -0,0 +1,18 @@
+﻿namespace GenericEcommerce.Models
+{
+    public class Game
+    {
+        public long IdGame { get; set; }
+        public string? Name { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? LongDescription { get; set; }
+        public string? ImageURL { get; set; }
+        public string? ImageURLThumbnail { get; set; }
+        public bool IsFavorite { get; set; }
+        public bool IsAvailable { get; set; }
+        public decimal Price { get; set; }
+        public long CategoryId { get; set; }
+
+        public Category? Category { get; set; }
+    }
+}
