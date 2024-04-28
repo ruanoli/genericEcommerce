65a799a160c4e6bebaeda6fb13c0cb14ca92d99e
Ruan Oliveira <ruanoliveira.pro@gmail.com>
2024-04-28 14:39:36 -0300

    game repository

---
+++ GenericEcommerce/Interfaces/IGameRepository.cs
@@ -0,0 +1,11 @@
+﻿using GenericEcommerce.Models;
+
+namespace GenericEcommerce.Interfaces
+{
+    public interface IGameRepository
+    {
+        IList<Game> Games { get; }
+        IList<Game> IsFavoritesGames { get; }
+        Game GetGameById(int id);
+    }
+}
---
+++ GenericEcommerce/Repositories/GameRepository.cs
@@ -0,0 +1,31 @@
+﻿using GenericEcommerce.Context;
+using GenericEcommerce.Interfaces;
+using GenericEcommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GenericEcommerce.Repositories
+{
+    public class GameRepository : IGameRepository
+    {
+        private readonly AppDbContext _context;
+
+        public GameRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Game> Games => _context.Games
+            .Include(x => x.Category)
+            .ToList();
+
+        public IList<Game> IsFavoritesGames => _context.Games
+            .Where(x => x.IsFavorite)
+            .Include(x => x.Category)
+            .ToList();
+
+        public Game GetGameById(int id)
+        {
+            return _context.Games.Where(x => x.GameId == id).FirstOrDefault();
+        }
+    }
+}
