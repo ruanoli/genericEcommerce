c20f5c4729d16ff5daeec0da332b6011ebbcf989
Ruan Oliveira <ruanoliveira.pro@gmail.com>
2024-06-27 00:19:13 -0300

    checkout

---
+++ GenericEcommerce/Controllers/OrderController.cs
@@ -0,0 +1,61 @@
+﻿using GenericEcommerce.Interfaces;
+using GenericEcommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenericEcommerce.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrderController(IOrderRepository orderRepository, ShoppingCart shoppingCart)
+        {
+            _orderRepository = orderRepository;
+            _shoppingCart = shoppingCart;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            int quantOrderItems = 0;
+            decimal totalValueOrderItems = 0;
+
+            IList<ItemShoppingCart> cartItems = _shoppingCart.GetItemsToCart();
+
+
+            if(_shoppingCart.Items.Count == 0)
+            {
+                ModelState.AddModelError("", "Carrinho de Compras vazio.");
+            }
+
+            foreach (var item in cartItems)
+            {
+                quantOrderItems += item.Quantity;
+                totalValueOrderItems += (item.Game.Price * item.Quantity);
+            }
+
+            order.TotalOrderItems = quantOrderItems;
+            order.TotalOrderValue = totalValueOrderItems;
+
+            if (ModelState.IsValid)
+            {
+                _orderRepository.CreateOrder(order);
+
+                ViewBag.CheckoutMessage = "Pedido realizado com sucesso!";
+                ViewBag.TotalOrder = _shoppingCart.GetShoppingCartTotal();
+
+                _shoppingCart.CleanCart();
+
+                return View("~/Views/CompleteCheckout.cshtml", order);
+            }
+
+            return View(order);
+        }
+    }
+}
--- GenericEcommerce/Models/Order.cs
+++ GenericEcommerce/Models/Order.cs
@@ -10,23 +10,36 @@ namespace GenericEcommerce.Models
 
         [Required(ErrorMessage = "Campo Obrigatório!")]
         [StringLength(50)]
+        [Display(Name = "Nome")]
         public string? Name { get; set; }
        
         [Required(ErrorMessage = "Campo Obrigatório!")]
         [StringLength(50)]
+        [Display(Name = "Sobrenome")]
         public string? Surname { get; set; }
+
+        [Display(Name = "Endereço")]
         public string? Address1 { get; set; }
+
+        [Display(Name = "Complemento")]
         public string? Address2 { get; set; }
+
+        [Display(Name = "Estado")]
         public string? State { get; set; }
+
+        [Display(Name = "Cidade")]
         public string? City { get; set; }
+
         [Display(Name = "CEP")]
         [StringLength(10, MinimumLength = 8)]
         public string? Cep { get; set; }
 
+        [Display(Name = "Número do Telefone")]
         [DataType(DataType.PhoneNumber)]
         [StringLength(11)]
         public string? Telephone { get; set; }
 
+        [Display(Name = "Email")]
         [DataType(DataType.EmailAddress)]
         [RegularExpression(@"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])",
             ErrorMessage = "O e-mail não possui um formato correto")]
